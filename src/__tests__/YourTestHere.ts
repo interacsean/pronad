a98e952423ffe68eb025e4d2ecec3e6c1a3a09e4
Sean H <seanhasselback@gmail.com>
2019-03-27 09:40:35 +1200

    testing pronad

--- src/__tests__/YourTestHere.ts
+++ src/__tests__/YourTestHere.ts
@@ -1,7 +1,41 @@
-import { yourFeatureHere } from '../index';
+import { monadifyPromises } from 'pronad';
 
-describe('Your test suite', () => {
-  it('shouldhave a test', (done) => {
-    done.fail();
+monadifyPromises();
+
+function createPromise<T>(resOrRej: boolean, val: T): Promise<T> {
+  return new Promise((res, rej) => {
+    setTimeout(() => (resOrRej ? res : rej)(val), 50);
+  });
+}
+
+describe('map method', () => {
+  it('should map on resolved promises', () => {
+    const result = createPromise(true, 5)
+      .map((resVal: number) => resVal * 2);
+
+    expect(result).resolves.toEqual(10);
+  });
+
+  it('should skip rejected promises', () => {
+    const result = createPromise(false, 5)
+      .map((resVal: number) => resVal * 2);
+    
+    expect(result).rejects.toEqual(5);
+  });
+});
+
+describe('leftMap method', () => {
+  it('should map on rejected promises', () => {
+    const result = createPromise(false, 5)
+      .leftMap((rejVal: number) => rejVal * 2);
+
+    expect(result).rejects.toEqual(10);
+  });
+
+  it('should skip resolved promises', () => {
+    const result = createPromise(true, 5)
+      .leftMap((rejVal: number) => rejVal * 2);
+    
+    expect(result).resolves.toEqual(5);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-import './types/YourTypeHere.d.ts';
+import './types/Promise';
+import { monadifyPromises, Pronad } from 'pronad';
 
-export const yourFeatureHere = () => {
+const pr: Pronad<Error, number> = Promise.resolve(5);
+
+monadifyPromises();
+
+function createPromise<T>(resOrRej: boolean, val: T): Promise<T> {
+  return new Promise((res, rej) => {
+    setTimeout(() => (resOrRej ? res : rej)(val), 50);
+  });
 }
+
+const result: Pronad<Error, string> = createPromise(true, 5)
+  .map((resVal: number) => resVal * 2)
+  .map((resVal: number) => {
+    console.log(resVal);
+    return 'hi';
+  });
+
+const result2: Pronad<string, string> = result.map(x => x);
